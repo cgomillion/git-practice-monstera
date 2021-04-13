81f9101d3cfc1861d2e7cb87300bed12ae767f38
Corey Gomillion <coreygomillion@Coreys-MacBook-Pro.local>
2021-04-13 19:03:40 -0400

    subtraction is working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const calculator = {
-    add:(num1, num2)=> num1 + num2
+    add:(num1, num2)=> num1 + num2,
+    sub:(num1, num2)=> num1 - num2
 }
 
 console.log(calculator.add(5,3))
+console.log(calculator.sub(5,3))
